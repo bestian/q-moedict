a5f771023b1279ef16a23da4464957689f08ccc0
Bestian Tang <bestian@gmail.com>
2020-11-08 16:36:24 +0800

    init

---
+++ src/pages/Word.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-page class="flex flex-center">
+    <h1>{{w}}</h1>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+  data () {
+    return {
+      w: null
+    }
+  },
+  mounted () {
+    this.w = this.$route.params.id
+    this.$axios.get('https://www.moedict.tw/c/' + this.w + '.json')
+      .then((response) => {
+        this.data = response.data
+      })
+  }
+}
+</script>
