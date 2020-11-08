7460d0663ee3518fd78fe26553305d8ef8003e74
Bestian Tang <bestian@gmail.com>
2020-11-08 16:28:36 +0800

    init

---
+++ src/pages/Index.vue
@@ -0,0 +1,29 @@
+<template>
+  <q-page class="flex flex-center">
+    <input type="search" v-model = "myKey" list = "words"/>
+    <!-- <datalist id="words">
+      <option v-for="d in data" :key="d" :value="d"/>
+    </datalist> -->
+    <div>
+        {{ myKey }}
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+  data () {
+    return {
+      myKey: '',
+      data: []
+    }
+  },
+  mounted () {
+    this.$axios.get('https://www.moedict.tw/c/index.json')
+      .then((response) => {
+        this.data = response.data
+      })
+  }
+}
+</script>
