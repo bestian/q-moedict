2c0a3205dba5d4f449a85d998a74e40d7070b9c5
Bestian Tang <bestian@gmail.com>
2020-11-17 10:15:09 +0800

    Update custom-service-worker.js

--- src-pwa/custom-service-worker.js
+++ src-pwa/custom-service-worker.js
@@ -1,5 +1,19 @@
 /*
  * This file (which will be your service worker)
- * is picked up by the build system ONLY if
- * quasar.conf > pwa > workboxPluginMode is set to "InjectManifest"
+ * is picked up by the build system if BOTH conditions are met:
+ *  - You are building for production
+ *  - quasar.conf > pwa > workboxPluginMode is set to "InjectManifest"
  */
+
+/* global workbox */
+if (workbox) {
+  workbox.precaching.precache(self.__precacheManifest)
+
+  self.addEventListener('fetch', function (event) {
+    event.respondWith(
+      caches.match(event.request).then(function (response) {
+        return response || fetch(event.request)
+      })
+    )
+  })
+}
